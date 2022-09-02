73e3636f91776648cdb595ee19dcdba651245c58
StephanieMenard <nena.stf7@gmail.com>
2022-09-02 10:07:28 +0200

    commit folder

---
+++ js/script.js
@@ -0,0 +1,38 @@
+console.log('hello');
+
+/*
+let imgOblog = document.getElementById('img-oblog');
+
+
+
+
+
+imgOblog.addEventListener('mouseenter', handleImgOblog);
+function handleImgOblog(){
+    console.log('clickééé');
+    imgOblog.style.display="none";
+}
+
+imgOblog.addEventListener('mouseleave', handleImgOblogOrigine);
+function handleImgOblogOrigine(){
+    console.log('clickééé');
+    imgOblog.style.display="block";
+  
+}
+
+*/
+
+
+
+/* Event mouseout*/ 
+/*
+raptorPaintLi.addEventListener('mouseleave', handleOrigineImage);
+function handleOrigineImage(){
+    console.log('clickééé');
+    imageRaptor.style.display="none";
+    imageEpoxy.style.display="none";
+    imageMate.style.display="none"
+    imageBrillantDirect.style.display="none";
+    imageLaquagePaillete.style.display="none";
+    imageFix.style.display="block";
+}*/
